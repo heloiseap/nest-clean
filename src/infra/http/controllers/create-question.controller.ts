53849058d18c181b15d6d5495aa3a4c21a7912df
Heloise <heloiseap@users.noreply.github.com>
2025-08-22 17:44:16 -0300

    ate Implementando QuestionsRepository

---
+++ src/infra/http/controllers/create-question.controller.ts
@@ -0,0 +1,61 @@
+import { Body, Controller, Post, Req, UseGuards, UsePipes } from "@nestjs/common";
+import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
+import { CurrentUser } from "@/infra/auth/current-user.decorator";
+import type { UserPayload } from "@/infra/auth/jwt.strategy";
+import z from "zod";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+// import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
+
+const createQuestionBodySchema = z.object({
+    title: z.string(),
+    content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+
+    // constructor(private prisma: PrismaService) { }
+    constructor( private createQuestion: CreateQuestionUseCase) {}
+
+    @Post()
+    async handle(
+        @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+        @CurrentUser() user: UserPayload) {
+
+        const { title, content } = body
+        const userId = user.sub
+
+        await this.createQuestion.execute({
+            title,
+            content,
+            authorId: userId,
+            attachmentsIds: []
+        })
+        // const slug = this.convertToSlug(title)
+
+        // await this.prisma.question.create({
+        //     data: {
+        //         authorId: userId,
+        //         title,
+        //         content,
+        //         slug
+        //     },
+        // })
+    }
+
+    // private convertToSlug(title: string): string {
+    //     return title
+    //         .toLowerCase()
+    //         .normalize("NFD")
+    //         .replace(/[\u0300-\u036f]/g,"")
+    //         .replace(/[^\w\s-]/g, "")
+    //         .replace(/\s+/g,"-")
+    // }
+
+}
